b2a859f70de8a890ab1b170a73a94e7966bf95b0
Rosangela <96451367+Rommendes@users.noreply.github.com>
2022-10-16 10:26:07 -0300

    Aplicando estilos

---
+++ javascript.js
@@ -0,0 +1,18 @@
+/*ESTA Function AO SER CLICADA MOSTRA O MENU, AO SER CLICADA NOVAMENTE, ESCONDE O MENU */
+function clickmenu (){
+    if(itens.style.display =='block'){
+        itens.style.display = 'none'
+    }else{
+         itens.style.display = 'block'
+    }
+}
+/*Esta função mostra o bloco de ítens (menu) se a tela for grande
+---------------------------------
+innerWidth => largura da tela */
+function mudouTamanho(){
+    if(window.innerWidth >= 768){
+        itens.style.display = 'block'
+    }else{
+        itens.style.display = 'none'
+    }
+}
